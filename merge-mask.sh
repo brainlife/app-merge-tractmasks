9ee832bb92ae7af01a5aead3e30a85d79a1bc146
Brad Caron <bacaron@utexas.edu>
2023-06-30 16:58:14 -0500

    reason: first code

---
+++ merge-mask.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+masks=`jq -r '.mask' config.json`
+names=(`jq -r '.[].name' ${masks}/color.json`)
+outdir="masks"
+[ ! -d ${outdir} ] && mkdir ${outdir}
+
+for i in ${names[*]}
+do
+    if [[ $i == ${names[0]} ]]; then
+        cp ${masks}/${i}*.nii.gz $outdir/mask.nii.gz
+    else
+        fslmerge -t $outdir/mask.nii.gz $outdir/mask.nii.gz ${masks}/$i*.nii.gz
+    fi
+done
+
+if [ ! -f $outdir/mask.nii.gz ]; then
+    echo "something went wrong. check data"
+    exit 1
+fi
+
