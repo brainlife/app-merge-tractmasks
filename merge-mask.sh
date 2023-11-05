f755bdf52fcb01a29dbe4b86e952e2f4228dd88b
Brad Caron <bacaron245@gmail.com>
2023-11-05 01:45:41 -0500

    Update merge-mask.sh

--- merge-mask.sh
+++ merge-mask.sh
@@ -3,21 +3,21 @@
 masks=`jq -r '.mask' config.json`
 names=(`jq -r '.[].name' ${masks}/color.json`)
 outdir="masks"
-[ ! -d ${outdir} ] && mkdir ${outdir}
+[ ! -d ${outdir} ] && mkdir ${outdir} ${outdir}/masks
 
 for i in ${names[*]}
 do
     if [[ $i == ${names[0]} ]]; then
         cp ${masks}/${i}*.nii.gz $outdir/mask.nii.gz
     else
-        fslmerge -t $outdir/mask.nii.gz $outdir/mask.nii.gz ${masks}/$i*.nii.gz
+        fslmerge -t $outdir/mask.nii.gz $outdir/masks/mask.nii.gz ${masks}/$i*.nii.gz
     fi
 done
 
-if [ ! -f $outdir/mask.nii.gz ]; then
+if [ ! -f $outdir/masks/mask.nii.gz ]; then
     echo "something went wrong. check data"
     exit 1
 fi
 
-[ ! -f ${outdir}/label.json ] && cp ${masks}/color.json ${outdir}/label.json
+[ ! -f ${outdir}/label.json ] && cp ${masks}/color.json ${outdir}/masks/label.json
 
